ec4cd79e5ac018852c19754f1aefa25ade9787fa
farrukh90 <farrukhsadykov@gmail.com>
2019-05-04 14:28:45 -0500

    python app added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,14 @@
+node {
+    stage("Clone repo"){
+        git "git@github.com:farrukh90/flask-examples.git"
+    }
+    stage("Install Requirements"){
+        sh "scp -r *   ec2-user@${ENV}:/tmp"
+        sh "ssh ec2-user@${ENV} virtualenv /tmp/venv"
+        sh "ssh ec2-user@${ENV} ./tmp/venv/activate"
+        sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
+    }
+    stage("Start python app"){
+        sh "ssh ec2-user@${ENV} python /tmp/01-hello-world/hello.py"
+    }
+}
