4fcf897ccbf00663174c6d4fbe5f799501a06d6c
farrukh90 <farrukhsadykov@gmail.com>
2019-05-04 14:43:08 -0500

    added virtualenv

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,6 +4,7 @@ node {
         git "git@github.com:farrukh90/flask-examples.git"
     }
     stage("Install Requirements"){
+        sh "ssh ec2-user@${ENV} sudo pip install virtualenv"
         sh "scp -r *   ec2-user@${ENV}:/tmp"
         sh "ssh ec2-user@${ENV} virtualenv /tmp/venv"
         sh "ssh ec2-user@${ENV} ./tmp/venv/activate"
